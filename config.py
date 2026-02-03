adcf92f630bc3d0a860eb1322296f73b3d2c1bfa
coderpagluu <chavdasujal08@gmail.com>
2026-02-03 12:38:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 # Bot configuration
-API_ID = YOUR_API_ID
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = 25933223
+API_HASH = "6ef5a426d85b7f01562a41e6416791d3"
+BOT_TOKEN = "8303906188:AAG1KrqFPpiOVSJzNNs18TsIsDHTXrS8gM4"
 
 # Command prefixes
 COMMAND_PREFIX = [",", ".", "/", "!"]
 
 # Developer details
-DEVELOPER_ID = YOUR_USER_ID  # Replace with the developer's actual user ID
-UPDATES_CHANNEL = "YOUR_CHANNEL_URL"  # URL of the updates channel
+DEVELOPER_ID = 7995790656  # Replace with the developer's actual user ID
+UPDATES_CHANNEL = "https://t.me/sujalupdate"  # URL of the updates channel
 
 ADMIN_IDS = [FIRST_ADMIN_ID, SECOND_ADMIN_ID]
 
 # MongoDB configuration
 MONGO_URL = "YOUR_MONGO_URL"
+
